3eee8640932d211298cd33bfe74cc870523e73a5
Omar Toledo <o.toledo@uniandes.edu.co>
2024-10-28 10:11:46 -0500

    Project Back init

---
+++ Back/students-grades/src/main/java/com/dsw/students_grades/StudentsGradesApplication.java
@@ -0,0 +1,13 @@
+package com.dsw.students_grades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsGradesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsGradesApplication.class, args);
+	}
+
+}
---
+++ Back/students-grades/src/test/java/com/dsw/students_grades/StudentsGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsw.students_grades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsGradesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
