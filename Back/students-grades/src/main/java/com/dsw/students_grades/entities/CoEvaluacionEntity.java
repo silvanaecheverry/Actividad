2d9c4b0026256cd672cbdd275237f02572c9f419
omardtl24 <o.toledo@uniandes.edu.co>
2024-10-28 23:32:43 -0500

    Date Podam Strategy

--- Back/students-grades/src/main/java/com/dsw/students_grades/entities/CoEvaluacionEntity.java
+++ Back/students-grades/src/main/java/com/dsw/students_grades/entities/CoEvaluacionEntity.java
@@ -4,15 +4,27 @@ import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamDoubleValue;
 import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.common.PodamStrategyValue;
+import uk.co.jemos.podam.common.PodamStringValue;
+import com.dsw.students_grades.podam.CustomDateStrategy;
 
 @Data
 @Entity
 public class CoEvaluacionEntity extends BaseEntity {
+    
+    @PodamDoubleValue(minValue = 0.0, maxValue = 5.0)
+    private Double calificacion;
 
-    private double calificacion;
+    @PodamStringValue
     private String comentario;
+
+    @Temporal(TemporalType.DATE)
+    @PodamStrategyValue(CustomDateStrategy.class)
     private Date fecha;
     
     @PodamExclude
